8e77f1dc47a2cc64a9d7c51738ac9034b8f57324
Special <weareallspecial@yahoo.com>
2014-11-08 00:57:02 +0100

    second commit command line

---
+++ mytest/src/de/fraz/hello/HelloGit2.java
@@ -0,0 +1,10 @@
+package de.fraz.hello;
+
+public class HelloGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello jjaaaaa Git2! V0");
+	}
+
+}
