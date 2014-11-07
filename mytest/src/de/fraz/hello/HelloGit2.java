9df8ada353291a40e03042b5379a52bf1179be46
FrankZuchowski <FrankZuchowski@users.noreply.github.com>
2014-11-08 00:59:57 +0100

    Update HelloGit2.java

--- mytest/src/de/fraz/hello/HelloGit2.java
+++ mytest/src/de/fraz/hello/HelloGit2.java
@@ -5,6 +5,7 @@ public class HelloGit2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello jjaaaaa Git2! V0");
+		System.out.println("Why not this way.....remote change");
 	}
 
 }
